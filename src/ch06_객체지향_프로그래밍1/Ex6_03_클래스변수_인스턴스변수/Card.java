fd82746c401fbffad878e09c9e0381d643a4957b
hyeonorof <hyeonorof@gmail.com>
2023-03-16 22:06:30 +0900

    클래스 변수 인스턴스 변수

---
+++ src/ch06_객체지향_프로그래밍1/Ex6_03_클래스변수_인스턴스변수/Card.java
@@ -0,0 +1,8 @@
+package ch06_객체지향_프로그래밍1.Ex6_03_클래스변수_인스턴스변수;
+
+class Card {
+    String kind;
+    int number;
+    static int width = 100;
+    static int height = 250;
+}
---
+++ src/ch06_객체지향_프로그래밍1/Ex6_03_클래스변수_인스턴스변수/Ex6_03.java
@@ -0,0 +1,28 @@
+package ch06_객체지향_프로그래밍1.Ex6_03_클래스변수_인스턴스변수;
+
+class Ex6_03 {
+    public static void main(String args[]) {
+        System.out.println("Card.width = " + Card.width);
+        System.out.println("Card.height = " + Card.height);
+
+        Card c1 = new Card();
+        c1.kind = "Heart";
+        c1.number = 7;
+
+        Card c2 = new Card();
+        c2.kind = "Spade";
+        c2.number = 4;
+
+        System.out.println("c1은 " + c1.kind + ", " + c1.number + "이며, 크기는 (" + c1.width + ", " + c1.height + ")");
+        System.out.println("c2는 " + c2.kind + ", " + c2.number + "이며, 크기는 (" + c2.width + ", " + c2.height + ")");
+        System.out.println("c1의 width와 height를 각각 50, 80으로 변경합니다.");
+
+        c1.width = 50;  // iv로 오해할 수 있기 때문에 클래스명.변수명으로 하는게 좋다
+        c1.height = 80; // Card.width; Card.height
+
+        System.out.println("c1은 " + c1.kind + ", " + c1.number + "이며, 크기는 (" + c1.width + ", " + c1.height + ")");
+        System.out.println("c2는 " + c2.kind + ", " + c2.number + "이며, 크기는 (" + c2.width + ", " + c2.height + ")");
+    }
+}
+
+
