c9722898a6381fd1fe648c59175b3203b087a05c
hyeonorof <hyeonorof@gmail.com>
2023-03-24 21:43:14 +0900

    접근제어자 2

---
+++ src/ch07_Object_Oriented_Programming2/Test05_Access_Modifier2/ParentTest2.java
@@ -0,0 +1,23 @@
+package ch07_Object_Oriented_Programming2.Test05_Access_Modifier2;
+
+import ch07_Object_Oriented_Programming2.Test04_Access_Modifier.Parent;
+
+class Child extends Parent {
+    public void printMembers() {
+//        System.out.println(prv); // 에러
+//        System.out.println(dft); // 에러
+        System.out.println(prt); // OK
+        System.out.println(pub); // OK
+    }
+}
+
+public class ParentTest2 {
+    public static void main(String[] args) {
+        Parent p = new Parent();
+
+//        System.out.println(p.prv); // 에러
+//        System.out.println(p.dft); // 에러
+//        System.out.println(p.prt); // 에러
+        System.out.println(p.pub); // OK
+    }
+}
