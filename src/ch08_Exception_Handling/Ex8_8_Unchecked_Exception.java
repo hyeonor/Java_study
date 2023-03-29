5f24fdf9e0b2d33bb8471b2f26c6feb65979f4a8
hyeonorof <hyeonorof@gmail.com>
2023-03-29 21:22:02 +0900

    unchecked 예외

---
+++ src/ch08_Exception_Handling/Ex8_8_Unchecked_Exception.java
@@ -0,0 +1,15 @@
+package ch08_Exception_Handling;
+
+/*
+checked 예외: 컴파일러가 예외 처리 여부를 체크(예외 처리 필수)
+unchecked 예외: 컴파일러가 예외 처리 여부를 체크 안 함(예외 처리 선택)
+ */
+
+class Ex8_8_Unchecked_Exception {
+    public static void main(String[] args) {
+        // 컴파일 OK
+        // unchecked 예외, 예외 처리 선택
+        // RuntimeException과 그 자손은 예외처리를 해주지 않아도 컴파일이 된다. (선택)
+        throw new RuntimeException();    // RuntimeException을 고의로 발생시킨다.
+    }
+}
