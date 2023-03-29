31a6b29416f9a1b579754aed6d50713bab650c52
hyeonorof <hyeonorof@gmail.com>
2023-03-29 21:17:00 +0900

    예외의 발생과 catch블럭 1

---
+++ src/ch08_Exception_Handling/Ex8_3_Exception_Occurred_And_try_catch.java
@@ -0,0 +1,17 @@
+package ch08_Exception_Handling;
+
+class Ex8_3_Exception_Occurred_And_try_catch {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+
+        try {
+            System.out.println(3);
+            System.out.println(0 / 0);
+            System.out.println(4); // 실행되지 않는다.
+        } catch (Exception e) {     // ArithmeticException 대신 Exception을 사용.
+            System.out.println(5);
+        } // try-catch의 끝
+        System.out.println(6);
+    } // main 메서드의 끝
+}
