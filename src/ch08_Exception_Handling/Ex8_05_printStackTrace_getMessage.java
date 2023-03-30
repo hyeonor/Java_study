4c00462f2c92f3bd24f24badcbbd0dcb57286e18
hyeonorof <hyeonorof@gmail.com>
2023-03-30 20:11:24 +0900

    printStackTrace(), getMessage()

--- src/ch08_Exception_Handling/Ex8_05_printStackTrace_getMessage.java
+++ src/ch08_Exception_Handling/Ex8_05_printStackTrace_getMessage.java
@@ -6,7 +6,7 @@ printStackTrace(): 예외발생 당시의 호출스택(Call Stack)에 있었던 
 getMessage():      발생한 예외클래스의 인스턴스에 저장된 메시지를 얻을 수 있다.
  */
 
-class Ex8_5_printStackTrace_getMessage {
+class Ex8_05_printStackTrace_getMessage {
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println(2);
