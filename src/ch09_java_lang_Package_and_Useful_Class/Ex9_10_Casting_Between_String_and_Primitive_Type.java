88dd6c21ee9f945be71c8e9e1ec91123f7c5707e
hyeonorof <hyeonorof@gmail.com>
2023-04-03 19:50:32 +0900

    문자열과 기본형 간의 변환

---
+++ src/ch09_java_lang_Package_and_Useful_Class/Ex9_10_Casting_Between_String_and_Primitive_Type.java
@@ -0,0 +1,17 @@
+package ch09_java_lang_Package_and_Useful_Class;
+
+class Ex9_10_Casting_Between_String_and_Primitive_Type {
+    public static void main(String[] args) {
+        int iVal = 100;
+        String strVal = String.valueOf(iVal); // int를 String으로 변환한다.
+
+        double dVal = 200.0;
+        String strVal2 = dVal + "";    // int를 String으로 변환하는 또 다른 방법
+
+        double sum = Integer.parseInt("+" + strVal) + Double.parseDouble(strVal2);
+        double sum2 = Integer.valueOf(strVal) + Double.valueOf(strVal2);
+
+        System.out.println(String.join("", strVal, "+", strVal2, "=") + sum);
+        System.out.println(strVal + "+" + strVal2 + "=" + sum2);
+    }
+}
