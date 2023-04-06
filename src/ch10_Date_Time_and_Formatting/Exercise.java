6ab966d8101f11b40d2d84e56707aaa3924cd0bd
hyeonorof <hyeonorof@gmail.com>
2023-04-06 21:55:24 +0900

    Calendar 클래스 getTimeInMillis() 2

--- src/ch10_Date_Time_and_Formatting/Exercise.java
+++ src/ch10_Date_Time_and_Formatting/Exercise.java
@@ -9,27 +9,27 @@ public class Exercise {
         System.out.println(getDayDiff("20010103", "200103"));
     }
 
-    static int getDayDiff(String yyyymmdd1, String yyyymmdd2) {
+    static int getDayDiff(String date1, String date2) {
         int diff = 0;
 
         try {
-            int year1 = Integer.parseInt(yyyymmdd1.substring(0, 4));
-            int month1 = Integer.parseInt(yyyymmdd1.substring(4, 6)) - 1;
-            int day1 = Integer.parseInt(yyyymmdd1.substring(6, 8));
-            int year2 = Integer.parseInt(yyyymmdd2.substring(0, 4));
-            int month2 = Integer.parseInt(yyyymmdd2.substring(4, 6)) - 1;
-            int day2 = Integer.parseInt(yyyymmdd2.substring(6, 8));
+            int year1 = Integer.parseInt(date1.substring(0, 4));
+            int month1 = Integer.parseInt(date1.substring(4, 6)) - 1;
+            int day1 = Integer.parseInt(date1.substring(6, 8));
+            int year2 = Integer.parseInt(date2.substring(0, 4));
+            int month2 = Integer.parseInt(date2.substring(4, 6)) - 1;
+            int day2 = Integer.parseInt(date2.substring(6, 8));
 
-            Calendar date1 = Calendar.getInstance();
-            Calendar date2 = Calendar.getInstance();
+            Calendar Calendar1 = Calendar.getInstance();
+            Calendar Calendar2 = Calendar.getInstance();
 
-            date1.clear();
-            date2.clear();
+            Calendar1.clear();
+            Calendar2.clear();
 
-            date1.set(year1, month1, day1);
-            date2.set(year2, month2, day2);
+            Calendar1.set(year1, month1, day1);
+            Calendar2.set(year2, month2, day2);
 
-            diff = (int) ((date1.getTimeInMillis() - date2.getTimeInMillis())) / (24 * 60 * 60 * 1000);
+            diff = (int) ((Calendar1.getTimeInMillis() - Calendar2.getTimeInMillis())) / (24 * 60 * 60 * 1000);
         } catch (Exception e) {
             e.printStackTrace();
         }
