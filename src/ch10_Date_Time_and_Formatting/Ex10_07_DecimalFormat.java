d2febf31923c4a01c302167e97011a34dddcfb7a
hyeonorof <hyeonorof@gmail.com>
2023-04-07 22:05:32 +0900

    DecimalFormat 클래스 2

---
+++ src/ch10_Date_Time_and_Formatting/Ex10_07_DecimalFormat.java
@@ -0,0 +1,22 @@
+package ch10_Date_Time_and_Formatting;
+
+import java.text.*;
+
+class Ex10_07_DecimalFormat {
+    public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("#,###.##");
+        DecimalFormat df2 = new DecimalFormat("#.###E0");
+
+        try {
+            Number num = df.parse("1,234,567.89");
+            System.out.print("1,234,567.89" + " -> ");
+
+            double d = num.doubleValue();
+            System.out.print(d + " -> ");
+
+            System.out.println(df2.format(num));
+        } catch (Exception e) {
+
+        }
+    }
+}
