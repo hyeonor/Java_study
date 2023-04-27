a9adcce89f0690bad3cab47fb858adf0aab9c419
hyeonorof <hyeonorof@gmail.com>
2023-04-27 20:52:00 +0900

    Generic, HashMap

--- src/ch12_Generic_Enum_Annotation/Ex12_02_Generic_HashMap.java
+++ src/ch12_Generic_Enum_Annotation/Ex12_02_Generic_HashMap.java
@@ -1,8 +1,8 @@
-package ch12_Generics_Enum_Annotation;
+package ch12_Generic_Enum_Annotation;
 
 import java.util.HashMap;
 
-class Ex12_02_Generics_HashMap {
+class Ex12_02_Generic_HashMap {
     public static void main(String[] args) {
         HashMap<String, Student2> map = new HashMap<>();
         map.put("자바왕", new Student2("자바왕", 1, 1, 100, 100, 100));
